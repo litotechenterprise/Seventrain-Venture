9776a6baba9abd49c999e4fa5fab9bcdbdaee490
Pablo <litotechenterprise@gmail.com>
2020-04-30 18:34:15 -0400

    added react

--- server.js
+++ server.js
@@ -3,9 +3,19 @@ var app = express();
 const PORT = process.env.PORT || 8080;
 
 app.get('/', function(req,res) {
-    res.status(200).send('Bitch Nigga');
+    res.status(200).send('Bitch Ass');
 });
+
+if(process.env.NODE_ENV == 'production') {
+    app.use(express.static('client/build'))
+
+    const path = require('path')
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html '))
+    })
+}
 
 app.listen(PORT, function(){
     console.log('Server is runing on PORT:', PORT);
 });
+
