a814123929bf2b57e12014666e537eee608126ac
Pablo <litotechenterprise@gmail.com>
2020-04-30 20:20:00 -0400

    ready for production with start

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const PORT = process.env.PORT || 8080;
 
 if(process.env.NODE_ENV == 'production') {
     app.use(express.static('client/build'))
-
     const path = require('path')
     app.get('*', (req,res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
